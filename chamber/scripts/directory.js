832019785a4323ae2e76cc66b2a2346e1088c0de
kevine-p1 <uwasekevine250@gmail.com>
2026-05-18 12:06:30 +0100

    first chamber commit

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,59 @@
+const membersContainer = document.querySelector("#members-container");
+
+const gridButton = document.querySelector("#grid-view");
+const listButton = document.querySelector("#list-view");
+
+async function getMembers() {
+    const response = await fetch("data/members.json");
+    const data = await response.json();
+
+    displayMembers(data);
+}
+
+function displayMembers(members) {
+
+    members.forEach((member) => {
+
+        const card = document.createElement("section");
+        card.classList.add("member-card");
+
+        card.innerHTML = `
+            <img src="${member.image}" alt="${member.name}" loading="lazy">
+
+            <h3>${member.name}</h3>
+
+            <p>${member.address}</p>
+
+            <p>${member.phone}</p>
+
+            <p>${member.description}</p>
+
+            <a href="${member.website}" target="_blank">
+                Visit Website
+            </a>
+
+            <p><strong>Membership Level:</strong> ${member.membership}</p>
+        `;
+
+        membersContainer.appendChild(card);
+    });
+}
+
+getMembers();
+
+gridButton.addEventListener("click", () => {
+    membersContainer.classList.add("grid-view");
+    membersContainer.classList.remove("list-view");
+});
+
+listButton.addEventListener("click", () => {
+    membersContainer.classList.add("list-view");
+    membersContainer.classList.remove("grid-view");
+});
+
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+});
